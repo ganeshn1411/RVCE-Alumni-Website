c797e82b6c0eff5d2d1fafa69db9ef4e0e1e4892
Ganesh N <ganeshn1411@gmail.com>
2014-06-21 00:38:35 +0530

    Create a_profile.php

---
+++ admin/a_profile.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<title>RVCE Alumni</title>
+		<meta name="description" content="Multi-Level Push Menu: Off-screen navigation with multiple levels" />
+		<meta name="keywords" content="multi-level, menu, navigation, off-canvas, off-screen, mobile, levels, nested, transform" />
+		<meta name="author" content="Codrops" />
+		<link rel="shortcut icon" href="../favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css1/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css1/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css1/icons.css" />
+		<link rel="stylesheet" type="text/css" href="css1/component.css" />
+		<link rel="stylesheet" type="text/css" href="../common.css" />
+		<script src="js1/modernizr.custom.js"></script>
+	<style>
+	
+	.fontchange{
+		margin: 0;
+		padding: 0.2in;
+		background: white;
+		color: #5f6f81;
+		list-style: none;
+		text-transform: none;
+		font-weight: 200;
+		font-family: Arial, sans-serif;
+		line-height: 60px;
+		font-size: 17px;
+	}
+	</style>
+	</head>
+	<body>
+	<div class="container">
+			<!-- Push Wrapper -->
+			<div class="mp-pusher" id="mp-pusher">
+
+				<!-- mp-menu -->
+				<nav id="mp-menu" class="mp-menu" >
+					<div class="mp-level" style="background-color:#FFFFFF;color:rgba(0,0,0,0.8);
+text-shadow: 0 0 1px rgba(0,0,0,0.1);">
+						<h2 class="icon" style="color:rgba(0,0,0,0.8);
+text-shadow: 0 0 1px rgba(0,0,0,0.1);">All Categories</h2>
+						<ul>
+							<li class="icon icon-arrow-left">
+								<a class="icon" href="#" style="color:rgba(0,0,0,0.8);
+text-shadow: 0 0 1px rgba(0,0,0,0.1);">Student Archive</a>
+<div class="mp-level" style="background-color:#FFFFFF; style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);" >
+									<h2 class="icon" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);">Student Archive</h2>
+									<ul>
+<li class="icon" ><a class="icon " href="student1.php" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px">Insert</a></li>
+<li class="icon" ><a class="icon " href="delstudent.php" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px">Delete</a></li>
+<li class="icon" ><a class="icon " href="#" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px">Update</a></li>	
+									</ul>
+								</div>
+							</li>
+							<li class="icon icon-arrow-left">
+								<a class="icon" href="#" style="color:rgba(0,0,0,0.8);
+text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:20px;">Activities</a>
+<div class="mp-level" style="background-color:#FFFFFF; style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);" >
+									<h2 class="icon" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);">Activities</h2>
+									<ul>
+<li class="icon" ><a class="icon " href="activity.php" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Insert</a></li>
+<li class="icon" ><a class="icon " href="#" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Delete</a></li>
+<li class="icon" ><a class="icon " href="#" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Update</a></li>	
+									</ul>
+								</div>
+							</li>
+							<li class="icon icon-arrow-left">
+								<a class="icon" href="#" style="color:rgba(0,0,0,0.8);
+text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:20px;">Events</a>
+<div class="mp-level" style="background-color:#FFFFFF; style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);" >
+									<h2 class="icon" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);">Events</h2>
+									<ul>
+<li class="icon" ><a class="icon " href="seminar.php" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Insert</a></li>
+<li class="icon" ><a class="icon " href="#" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Delete</a></li>
+<li class="icon" ><a class="icon " href="#" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Update</a></li>	
+									</ul>
+								</div>
+							</li>
+							<li class="icon icon-arrow-left">
+								<a class="icon" href="#" style="color:rgba(0,0,0,0.8);
+text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:20px;">Contributions</a>
+<div class="mp-level" style="background-color:#FFFFFF; style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);" >
+									<h2 class="icon" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);">Contributions</h2>
+									<ul>
+<li class="icon" ><a class="icon " href="fund.php" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Insert</a></li>
+<li class="icon" ><a class="icon " href="#" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Delete</a></li>
+<li class="icon" ><a class="icon " href="#" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Update</a></li>	
+									</ul>
+								</div>
+							</li><li class="icon icon-arrow-left">
+								<a class="icon" href="#" style="color:rgba(0,0,0,0.8);
+text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:20px;">Acheivements</a>
+<div class="mp-level" style="background-color:#FFFFFF; style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);" >
+									<h2 class="icon" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);">Acheivements</h2>
+									<ul>
+<li class="icon" ><a class="icon " href="achievements.php" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Insert</a></li>
+<li class="icon" ><a class="icon " href="#" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Delete</a></li>
+<li class="icon" ><a class="icon " href="#" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Update</a></li>	
+									</ul>
+								</div>
+							</li>
+							<li class="icon icon-arrow-left">
+<a class="icon" href="#" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:20px;">Visit</a>
+<div class="mp-level" style="background-color:#FFFFFF; style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);" >
+									<h2 class="icon" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);">Visit</h2>
+									<ul>
+<li class="icon" ><a class="icon " href="visit.php" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Insert</a></li>
+<li class="icon" ><a class="icon " href="#" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Delete</a></li>
+<li class="icon" ><a class="icon " href="#" style="color:rgba(0,0,0,0.8); text-shadow: 0 0 1px rgba(0,0,0,0.1);font-size:18px;">Update</a></li>	
+									</ul>
+								</div>
+							</li>							
+					</div>
+				</nav>
+	<div class="scroller"><!-- this is for emulating position fixed of the nav -->
+					<div class="scroller-inner">
+						<!-- Top Navigation -->
+						<div class="codrops-top clearfix" style="height:60px; background-color:#FFFFFF; z-index=10">
+						<a href="./a_profile.php" style="position:relative;left:20px;border-right-width:thin;border-color:#5f6f81;border-right-style:solid;"><span class="fontchange">Home</span></a>
+						<span class="right"><a href="../logout.php" style="position:relative;left:-0px;border-left-width:thin;border-color:#5f6f81;border-left-style:solid">
+						<span class="fontchange">Logout</span></a></span>
+						</div>
+						<header>
+						<div class="container1">
+						<div>
+						<h2 style="text-align:center">WELCOME TO ADMIN HOME PAGE</h2>
+		<br>
+		<img src="../images/download.jpg" style="float:left; padding: 20px;"/>
+		<p>You have the following privilages<br>
+		<ul>
+		<li>Insert, Delete or Modify Alumni Records</li>
+		<li>Insert, Delete or Modify Activities</li>
+		<li>Insert, Delete or Modify Events</li>
+		<li>Insert, Delete or Modify Contributions</li>
+		<li>Insert, Delete or Modify Achievements</li>
+		<li>Insert, Delete or Modify Alumni Visit</li>
+		</ul>
+		Do explore all the feautures
+		</p></div></div>
+						</header>
+						<div class="content clearfix" >
+							<div class="block block-40 clearfix" >
+								<p><a href="#" id="trigger" class="menu-trigger" style="color:#000000;position:absolute;top:80px;left:50px;"></a></p>
+								</div>
+							</div>
+					</div><!-- /scroller-inner -->
+				</div><!-- /scroller -->
+
+			</div><!-- /pusher -->
+		</div><!-- /container -->
+		<script src="js1/classie.js"></script>
+		<script src="js1/mlpushmenu.js"></script>
+		<script>
+			new mlPushMenu( document.getElementById( 'mp-menu' ), document.getElementById( 'trigger' ) );
+		</script>
+		
+		
+	</body>
+</html>
+				
